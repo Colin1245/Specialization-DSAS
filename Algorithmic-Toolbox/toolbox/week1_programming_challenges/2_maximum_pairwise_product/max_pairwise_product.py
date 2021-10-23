7547b372115b2d695f3f015a2d35efdb0a6ddd1d
Weihang Li <weihang.li@tum.de>
2021-10-23 21:35:11 +0200

    update

--- Algorithmic-Toolbox/toolbox/week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.py
+++ Algorithmic-Toolbox/toolbox/week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.py
@@ -8,8 +8,23 @@ def max_pairwise_product(numbers):
 
     return max_product
 
+def max_pairwise_product_Fast(numbers):
+    n = len(numbers)
+
+    max_index1 = -1
+    for i in range(n):
+        if (max_index1 == -1) | (numbers[i] > numbers[max_index1]):
+            max_index1 = i
+
+    max_index2 = -1
+    for j in range(n):
+        if (j != max_index1) & ((max_index2 == -1) | (numbers[j] > numbers[max_index2])):
+            max_index2 = j
+
+
+    return ((numbers[max_index1])) * numbers[max_index2]
 
 if __name__ == '__main__':
     input_n = int(input())
     input_numbers = [int(x) for x in input().split()]
-    print(max_pairwise_product(input_numbers))
+    print(max_pairwise_product_Fast(input_numbers))
