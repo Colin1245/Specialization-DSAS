d1772ec4e0fa10e950702d57b218ce4142ef9339
Weihang Li <weihang.li@tum.de>
2021-10-24 15:29:45 +0200

    update

--- Algorithmic-Toolbox/toolbox/week2_algorithmic_warmup/5_fibonacci_number_again/fibonacci_huge.cpp
+++ Algorithmic-Toolbox/toolbox/week2_algorithmic_warmup/5_fibonacci_number_again/fibonacci_huge.cpp
@@ -18,17 +18,40 @@ long long get_fibonacci_huge_naive(long long n, long long m) {
     return current % m;
 }
 
+long long pisano(long long m){
+    long long res = 0;    
+    long long a = 0, b = 1, c = a + b;
+
+    for (long long i = 0; i < m * m; i++)
+    {
+        c = (a + b) % m;
+        a = b;
+        b = c;
+        if (a == 0 && b == 1)
+        {
+            res = i + 1;
+        }
+        
+    }
+    // std::cout << "res: " << res << std::endl;
+    return res;
+}
+
 long long get_fibonacci_huge_fast(long long n, long long m) {
+    long long pisano_period = pisano(m);
+    n = n % pisano_period;
+
     std::vector<long long> fib{0, 1};
 
     for (int i = 2; i <= n; i++){
-        int value = fib.at(i - 2) % m + fib.at(i - 1) % m;
+        long long value = fib.at(i - 2) % m + fib.at(i - 1) % m;
         fib.push_back(value);
     }
 
     if (n == 0){return 0;}
     else if (n == 1){return 1;}
-    else{return (fib.back()) % m;} 
+    else{return (fib.back()) % m;}    
+
 }
 
 int main() {
