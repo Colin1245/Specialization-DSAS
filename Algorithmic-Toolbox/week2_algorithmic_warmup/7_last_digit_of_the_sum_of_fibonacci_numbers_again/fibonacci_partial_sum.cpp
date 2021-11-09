23588f9b02f50abee4f4416d24df28dc95aecb12
Weihang Li <weihang.li@tum.de>
2021-11-09 21:58:05 +0100

    update week2

--- Algorithmic-Toolbox/week2_algorithmic_warmup/7_last_digit_of_the_sum_of_fibonacci_numbers_again/fibonacci_partial_sum.cpp
+++ Algorithmic-Toolbox/week2_algorithmic_warmup/7_last_digit_of_the_sum_of_fibonacci_numbers_again/fibonacci_partial_sum.cpp
@@ -21,8 +21,62 @@ long long get_fibonacci_partial_sum_naive(long long from, long long to) {
     return sum % 10;
 }
 
+long long pisano(long long m){
+    long long res = 0;    
+    long long a = 0, b = 1, c = a + b;
+
+    for (long long i = 0; i < m * m; i++)
+    {
+        c = (a + b) % m;
+        a = b;
+        b = c;
+        if (a == 0 && b == 1)
+        {
+            res = i + 1;
+        }
+        
+    }
+    
+    return res;
+}
+
+long long get_fibonacci_huge_fast(long long n, long long m) {
+    long long pisano_period = pisano(m);
+    n = n % pisano_period;
+
+    std::vector<long long> fib{0, 1};
+
+    for (int i = 2; i <= n; i++){
+        long long value = fib.at(i - 2) % m + fib.at(i - 1) % m;
+        fib.push_back(value);
+    }
+
+    if (n == 0){return 0;}
+    else if (n == 1){return 1;}
+    else{return (fib.back()) % m;}    
+
+}
+
+int fibonacci_sum_fast(long long n, long sum = 0)
+{
+    // S(n) = F(n+2) – 1 
+    return (get_fibonacci_huge_fast(n + 2, 10) - 1) % 10;
+}
+
+long long get_fibonacci_partial_sum_fast(long long from, long long to) {
+    long long F_to = 0;
+    long long F_from = 0;
+    F_from = fibonacci_sum_fast(from - 1);
+    F_to = fibonacci_sum_fast(to);
+    return F_to - F_from;
+}
+
 int main() {
     long long from, to;
     std::cin >> from >> to;
-    std::cout << get_fibonacci_partial_sum_naive(from, to) << '\n';
+    int sum = get_fibonacci_partial_sum_fast(from, to);
+    int value = sum < 0 ? (sum + 10) : sum;
+    std::cout << value << std::endl;
+
+    // std::cout << get_fibonacci_partial_sum_naive(from, to) << '\n';
 }
--- Algorithmic-Toolbox/week2_algorithmic_warmup/8_last_digit_of_the_sum_of_squares_of_fibonacci_numbers/fibonacci_sum_squares.cpp
+++ Algorithmic-Toolbox/week2_algorithmic_warmup/8_last_digit_of_the_sum_of_squares_of_fibonacci_numbers/fibonacci_sum_squares.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 int fibonacci_sum_squares_naive(long long n) {
     if (n <= 1)
@@ -18,8 +19,54 @@ int fibonacci_sum_squares_naive(long long n) {
     return sum % 10;
 }
 
+long long pisano(long long m){
+    long long res = 0;    
+    long long a = 0, b = 1, c = a + b;
+
+    for (long long i = 0; i < m * m; i++)
+    {
+        c = (a + b) % m;
+        a = b;
+        b = c;
+        if (a == 0 && b == 1)
+        {
+            res = i + 1;
+        }
+        
+    }
+    // std::cout << "res: " << res << std::endl;
+    return res;
+}
+
+long long get_fibonacci_huge_fast(long long n, long long m) {
+    long long pisano_period = pisano(m);
+    n = n % pisano_period;
+
+    std::vector<long long> fib{0, 1};
+
+    for (int i = 2; i <= n; i++){
+        long long value = fib.at(i - 2) % m + fib.at(i - 1) % m;
+        fib.push_back(value);
+    }
+
+    if (n == 0){return 0;}
+    else if (n == 1){return 1;}
+    else{return (fib.back()) % m;}    
+
+}
+
+// For Sum of Squares of Fibonacci Numbers, (F0^2 + ... + Fn^2) = Fn * Fn+1 = Fn * (Fn-1 + Fn)
+int fibonacci_sum_squares_fast(long long n) {
+    int Fn = get_fibonacci_huge_fast(n, 10);
+    int Fn_1 = get_fibonacci_huge_fast(n - 1, 10);
+    int result = (Fn * (Fn + Fn_1)) % 10;
+    int value = result < 0 ? (result + 10) : result;
+    return value;
+}
+
 int main() {
     long long n = 0;
     std::cin >> n;
-    std::cout << fibonacci_sum_squares_naive(n);
+    // std::cout << get_fibonacci_last_digit_fast(n) << std::endl;
+    std::cout << fibonacci_sum_squares_fast(n) << std::endl;
 }
