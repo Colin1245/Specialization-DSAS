e7b16ced797952c0e56bc59ea688c246a680e0a0
Weihang Li <weihang.li@tum.de>
2021-11-10 02:14:14 +0100

    update week3

--- Algorithmic-Toolbox/week3_greedy_algorithms/1_money_change/change.cpp
+++ Algorithmic-Toolbox/week3_greedy_algorithms/1_money_change/change.cpp
@@ -2,7 +2,24 @@
 
 int get_change(int m) {
   //write your code here
-  return n;
+  int sum = 0;
+  int rem = m;
+
+  if (m > 9){
+    rem = m % 10;
+    sum = sum + m / 10;
+  }
+  
+  else if (rem > 4){
+    rem = rem % 5;
+    sum++;
+  }
+
+  else if (rem > 0){
+    sum = sum + rem / 1;
+  }
+  
+  return sum;
 }
 
 int main() {
--- Algorithmic-Toolbox/week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.cpp
+++ Algorithmic-Toolbox/week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.cpp
@@ -1,12 +1,40 @@
 #include <iostream>
 #include <vector>
+#include <algorithm> 
+#include <numeric> 
 
 using std::vector;
 
 double get_optimal_value(int capacity, vector<int> weights, vector<int> values) {
   double value = 0.0;
+  int n = weights.size();
 
   // write your code here
+  vector<double> valweight = {0.0, 0.0, 0.0};
+  for (int i = 0; i < n; i++){
+    valweight.at(i) = values.at(i) / weights.at(i);
+  }
+
+  // for (auto elem : valweight){
+  //   std::cout << elem << " ";
+  // }
+
+  vector<int> indice(n);
+  std::iota(indice.begin(),indice.end(),0); //Initializing
+  std::sort(indice.begin(),indice.end(), [&](int i,int j){return valweight[i]<valweight[j];});
+
+  // for (auto elem : indice){
+  //   std::cout << elem << " ";
+  // }
+
+  while (capacity - weights.at(indice.back()) > 0){
+    value += values.at(indice.back());
+    capacity -= weights.at(indice.back());
+    indice.pop_back();
+    // std::cout << capacity << " " << value << std::endl;
+  }
+  
+  value += (capacity / weights.at(indice.back())) * values.at(indice.back());
 
   return value;
 }
